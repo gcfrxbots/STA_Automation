38301602059f6d2bc6029e92c661a492c9d625f8
Grant Fowler <gcf.rxbots@gmail.com>
2025-08-26 12:38:58 -0400

    Add files via upload

--- RoutineRun.py
+++ RoutineRun.py
@@ -1391,20 +1391,6 @@ if __name__ == "__main__":
     elif len(sys.argv) >= 7:
         print("Using command line arguments for API keys")
         _, shipstationAPIKey, shipstaionAPISecret, UPSAuthID, UPSAuthPass, openWeatherAPIKey, SquarespaceAPIKey = sys.argv[:7]
-        
-        # Insert -=-=-=- into middle of each key to prevent Jenkins masking
-        def insertSeparator(value):
-            if len(value) <= 10:
-                return value
-            mid = len(value) // 2
-            return value[:mid] + "-=-=-=-" + value[mid:]
-        
-        print("shipstationAPIKey:", insertSeparator(shipstationAPIKey))
-        print("shipstaionAPISecret:", insertSeparator(shipstaionAPISecret))
-        print("UPSAuthID:", insertSeparator(UPSAuthID))
-        print("UPSAuthPass:", insertSeparator(UPSAuthPass))
-        print("openWeatherAPIKey:", insertSeparator(openWeatherAPIKey))
-        print("SquarespaceAPIKey:", insertSeparator(SquarespaceAPIKey))
     else:
         print("No config file found and insufficient command line arguments provided.")
         print("Please either:")
